9b002aaa800fa21fe9e85bd67936d62db9c04966
taiji9203 <taiji9203@naver.com>
2022-11-24 16:29:21 +0900

    저장1

---
+++ java_project/src/ch03_test_1018/Hello.java
@@ -0,0 +1,11 @@
+package ch03_test_1018;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		System.out.println("Hello 이상용 자바");
+	}
+
+}
